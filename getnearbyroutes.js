3456ba5d01e531a7fcafe16fb50e3d657aa0ee5d
sidetrackedmind <malnorba@gmail.com>
2025-01-22 07:02:00 -0800

    something is wrong with the get routes function..

--- getnearbyroutes.js
+++ getnearbyroutes.js
@@ -1,31 +1,26 @@
 // Description: This file contains the function to get nearby routes from the Trimet API.
 
 function getNearbyRoutes(fromPlace) {
-    // fromPlace is a string with the format "lat,lon"
-    const baseUrl = "https://developer.trimet.org/ws/v1/stops";
-  
-    const params = new URLSearchParams({
-        json: true,
-        appId: "8CBD14D520C6026CC7EEE56A9",
-      showRoutes: true,
-      meters: 1200,
-      ll: fromPlace
+  // fromPlace is a string with the format "lat,lon"
+  const baseUrl = "https://developer.trimet.org/ws/v1/stops";
+
+  const params = new URLSearchParams({
+      json: true,
+      appId: "8CBD14D520C6026CC7EEE56A9",
+    showRoutes: true,
+    meters: 1200,
+    ll: fromPlace
+  });
+  const routeNumbers = new Set();
+  var xhReq = new XMLHttpRequest();
+  xhReq.open("GET", `${baseUrl}?${params}`, false);
+  xhReq.send(null);
+  var raw_response = JSON.parse(xhReq.responseText);
+  raw_response.resultSet.location.forEach(loc => {
+    loc.route.forEach(route => {
+      routeNumbers.add(route.route);
+      console.log(route.route);
     });
-  
-    return new Promise((resolve, reject) => {
-        fetch(`${baseUrl}?${params}`)
-          .then(response => response.json())
-          .then(data => {
-            const routeNumbers = new Set();
-            data.resultSet.location.forEach(loc => {
-              loc.route.forEach(route => {
-                routeNumbers.add(route.route);
-              });
-            });
-            resolve(routeNumbers); // Resolve the promise with routeNumbers
-          })
-          .catch(error => {
-            reject(error); // Reject the promise with error
-          });
-      });
-    }
+  });
+  return routeNumbers;
+  }
